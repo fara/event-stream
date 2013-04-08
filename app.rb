60ee8632296e1e751608bf2c375d9e0ccd025f46
Fernando Aramendi <fernando@devartis.com>
2013-04-07 23:52:12 -0300

    styles

--- app.rb
+++ app.rb
@@ -5,18 +5,19 @@ logger.level = Logger::WARN
 STREAMING_URL = 'https://stream.twitter.com/1.1/statuses/filter.json'
 TWITTER_USERNAME = ENV['TWITTER_USERNAME']
 TWITTER_PASSWORD = ENV['TWITTER_PASSWORD']
-TRACK_WORD = "claudioubeda"
+TRACK_WORD = "#claudioubeda"
 
 configure do
   if ENV['MONGOHQ_URL']
     uri = URI.parse(ENV['MONGOHQ_URL'])
-    conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+    conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])    
     DB = conn.db(uri.path.gsub(/^\//, ''))
+    DB.create_collection("tweets", :capped => true, :size => 10485760)
   else
     DB = Mongo::Connection.new.db("event-stream")
-  end
+    DB.create_collection("tweets", :capped => true, :size => 1048576)
+  end  
   
-  DB.create_collection("tweets", :capped => true, :size => 10485760)
 end
 
 set :public_folder, 'public'
